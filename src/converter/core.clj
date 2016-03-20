d3b40efbcdc1cd0a219df8167952695fd9534fce
liuchen <liuchen@zrcaifu.com>
2016-03-20 13:57:54 +0800

    finish read file

--- src/converter/core.clj
+++ src/converter/core.clj
@@ -1,7 +1,18 @@
 (ns converter.core
-  (:gen-class))
+  (:gen-class)
+  (:use clojure.java.io)
+  (:import [java.io File]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn read-and-write [name]
+  (let [file (File. name)]
+    (if-not (.isDirectory file)
+      (println (.getName file))
+      (doseq [x (.listFiles file)]
+        (println (.getName x))
+        (read-and-write (str file "/" (.getName x))))
+      )))
